4d578cc9bc1f5da35fcf572fa839905604d29e4d
Chu Yanshuo <chu@yanshuo.name>
2017-04-04 11:57:05 +0800

    laptop add utils functions

--- lib/PSSP/densities.py
+++ lib/PSSP/densities.py
@@ -1,7 +1,14 @@
 '''
-Created on 2012-11-21
-
-@author: andrew
+# =============================================================================
+#      FileName: densities.py
+#          Desc:
+#        Author: Chu Yanshuo
+#         Email: chu@yanshuo.name
+#      HomePage: http://yanshuo.name
+#       Version: 0.0.1
+#    LastChange: 2017-04-03 13:11:45
+#       History: @author: andrew
+# =============================================================================
 '''
 from multiprocessing import Pool
 from collections import namedtuple
@@ -9,7 +16,9 @@ from collections import namedtuple
 from pydp.densities import Density, log_binomial_pdf, log_poisson_pdf
 # from pydp.utils import log_sum_exp
 
-from utils import get_loga
+from utils import get_loga, get_cn_allele_config
+
+import constants
 
 
 FragmentSampledData = namedtuple(
@@ -17,10 +26,13 @@ FragmentSampledData = namedtuple(
 
 
 class FragmentSampledDensity(Density):
+
     def __init__(self, baseline=0, max_copy_number=7, params=None):
         Density.__init__(self, params=None)
-        self.baseline = baseline
-        self.max_copy_number = max_copy_number
+
+        self._baseline = baseline
+        self._max_copy_number = max_copy_number
+        self._allele_config = get_cn_allele_config(max_copy_number)
 
     def log_p(self, data, params):
         # params.phi
@@ -31,11 +43,10 @@ class FragmentSampledDensity(Density):
 
         if data.BASELINE == "True":
             copy_numbers = [2]
-        elif get_loga(data) < self.baseline:
-            copy_numbers = range(2, self.max_copy_number + 1)
+        elif get_loga(data) < self._baseline:
+            copy_numbers = range(2, self._max_copy_number + 1)
         else:
             copy_numbers = range(0, 2 + 1)
-
 
         ll = ll + self._log_RD() + self._log_BAF()
 
@@ -58,17 +69,36 @@ class FragmentSampledDensity(Density):
 
     def _getLLSeg(self, data, copy_number, phi):
         # get the likelihood of segment and it genotype
-        self._getRD(data, copy_number, phi)
+        ll_seg = 0
 
-        genotype, mu = getAlleleConfig(copy_number)
+        ll_rd= self._getRD(data, copy_number, phi)
+
+        allele_types = self._allele_config[copy_number]
+        ll_baf, pi = self._getBAF(data, copy_number, allele_types, phi)
+
+        ll_seg = ll_baf + ll_rd
 
         return ll_seg, pi
 
     def _getRD(self, data, copy_number, phi):
-        bar_c = phi * copy_number + (1.0 - phi) * 2.0
-        lambda_possion = (bar_c / 2.0) * baseline * (data.DN + 1) - 1
+        c_N = constants.COPY_NUMBER_NORMAL
+        bar_c = phi * copy_number + (1.0 - phi) * c_N
+        lambda_possion = (bar_c / c_N) * baseline * (data.DN + 1) - 1
         ll_RD = log_poisson_pdf(data.DT, lambda_possion)
         return ll_RD
+
+    def _getBAF(self, data, copy_number, allele_types, phi):
+        c_N = constants.COPY_NUMBER_NORMAL
+        mu_N = constants.MU_N
+        mu_G = allele_types.keys()
+        mu_E = get_mu_E_joint(mu_N, mu_G, c_N, copy_number, phi)
+        a_T_j = data.BAF[:, 2]
+        b_T_j = data.BAF[:, 3]
+        d_T_j = a_T_j + b_T_j
+        # add prior or not?
+        ll = log_binomial_likelihood_joint(b_T_j, d_T_j, mu_E)
+        ll_LOH_j = np.logaddexp.reduce(ll, axis=3).sum(axis=0)
+        return ll_LOH_j
 
     def _log_binomial_likelihood(self, b, d, phi, s, eps, mu_v, cn_r, cn_v):
         cn_N = 2
--- lib/PSSP/utils.py
+++ lib/PSSP/utils.py
@@ -17,6 +17,8 @@
 import numpy as np
 from scipy.special import gammaln
 
+from constants import EMPIRI_AAF, EMPIRI_BAF
+
 
 def get_loga(data):
     return np.log(data.DT + 1) - np.log(data.DN + 1)
@@ -28,19 +30,29 @@ def log_poisson_likelihood(k, Lambda):
 
 def get_cn_allele_config(max_copynumber):
     cn_allele_config = {}
-
     for cn in range(0, max_copynumber + 1):
         allele_config = {}
         for M_num in range(0, cn + 1):
             P_num = cn - M_num
             if P_num == 0 and M_num == 0:
-                pi_T = ''
-                mu_T = -1
-            else:
+                mu_T = EMPIRI_BAF / (EMPIRI_AAF + EMPIRI_BAF)
+                pi_T = 'NULL'
+            elif P_num == M_num:
+                mu_T = 0.5
                 pi_T = 'P'*P_num + 'M'*M_num
+            else:
                 mu_T = (P_num * 1.0) / cn
-
+                pi_T = 'P'*P_num + 'M'*M_num + '/' + 'P'*M_num + 'M'*P_num
             allele_config[pi_T] = mu_T
         cn_allele_config[cn] = allele_config
-
     return cn_allele_config
+
+
+def get_mu_E_joint(mu_N, mu_G, c_N, c_H, phi):
+    axis_1_shape = (phi.size, 1, 1)
+    axis_2_shape = (1, c_H.size, 1)
+    axis_3_shape = (1, 1, mu_G.size)
+    phi = phi.reshape(axis_1_shape)
+    c_H = c_H.reshape(axis_2_shape)
+    mu_G = mu_G.reshape(axis_3_shape)
+    return ((1.0 - phi)*c_N*mu_N + phi*c_H*mu_G)/((1.0 - phi)*c_N + phi*c_H)
