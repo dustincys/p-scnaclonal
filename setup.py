5522dab438414ad09f95805d653979e7bb6089f5
dustin lab <yanshuoc@gmail.com>
2017-04-10 16:56:10 +0800

    add main function

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+      name='PSSP',
+      version='0.0.0',
+      description='',
+      author='',
+      author_email='',
+      url='',
+      package_dir={'': 'lib'},
+      packages=['PSSP'],
+      scripts=['PSSP']
+     )
